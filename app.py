5a0e5a3fdce376c8c74ab7526cfc2e7cf62c8215
IAYADIDATA <nour.abdelmalek@fiparco.fr>
2023-09-13 22:31:16 +0200

    add comments

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def post_api():
     wind = fuels["wind(%)"]
     powerplants = payload["powerplants"]
 
+    # Calculate cost of the fuels of each powerplan
+    # And cost of the CO2
     for i in range(len(powerplants)):
         if powerplants[i]["type"] == "gasfired":
             cost = fuels["gas(euro/MWh)"]
@@ -26,6 +28,7 @@ def post_api():
             powerplants[i]["cost"] = 0
             powerplants[i]["co2"] = 0
 
+    # Sort powerplants by efficiency then cost then co2
     powerplants = sorted(
             powerplants, 
             key = lambda x: (x["efficiency"], -x["cost"], -x["co2"]),
@@ -36,21 +39,26 @@ def post_api():
     value = 0
     result = []
 
+    # Identify which powerplants we will use 
+    # Calculate Quantity for each one
     for i in range(len(powerplants)):
-
+        # Multiply the pmax by the percentage of wind
+        # to calculate the exact power can be produced
         value = powerplants[i]["pmax"]
         if powerplants[i]["type"] == "windturbine":
             value = round(value * (wind/100),2)
-
+        # Use the powerplant if the pmax is not above the load
         if (total + value) <= load:
             powerplants[i]["use"] = True
             powerplants[i]["quantity"] = value
             total += value
+            # Add a new item that contains the name and the quantity p
             item = {
                 "name" : powerplants[i]["name"],
                 "p": value
             }
             result.append(item)
+        # Use the powerplant if the pmin >= the rest to reach the load
         elif total + powerplants[i]["pmin"] <= load:
             value = load - total
             powerplants[i]["use"] = True
@@ -61,6 +69,7 @@ def post_api():
                 "p": value
             }
             result.append(item)
+        # Unusefull powerplant
         else:
             powerplants[i]["use"] = False
             powerplants[i]["quantity"] = 0
