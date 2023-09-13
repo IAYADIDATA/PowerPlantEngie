37141629314f561ba11e15eec69664b365b3843c
IAYADIDATA <nour.abdelmalek@fiparco.fr>
2023-09-13 23:19:12 +0200

    add exeptions

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 
 @app.route(rule="/productionplan", methods=['POST'])
 def post_api():
-    payload = request.json
-    result = calculate(payload)
-    return jsonify(result)
-
+    try:
+        payload = request.json
+        result = calculate(payload)
+        return jsonify(result)
+    except Exception as ex:
+        response = {
+                'status_code': 500,
+                'status': 'Internal Server Error'+ ex
+        }
+        return jsonify(response), 500
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888)
 
